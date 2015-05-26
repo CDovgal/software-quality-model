fe243ced600e8ed22868b0a0d48415c781aae6eb
Kostiantyn Dovgal <k.dovgal@external.oberthur.com>
2015-05-26 10:04:26 +0300

    Add static linking of dlib

--- src/software-quality-model.cpp
+++ src/software-quality-model.cpp
@@ -10,6 +10,11 @@
 #include "software-quality-model.h"
 #include "ui_software-quality-model.h"
 
+#include <dlib/bayes_utils.h>
+#include <dlib/graph_utils.h>
+#include <dlib/graph.h>
+#include <dlib/directed_graph.h>
+
 #include <algorithm>
 #include <string>
 
@@ -19,6 +24,9 @@ SQM_client::SQM_client(QWidget* ip_parent)
     , mp_ui(std::make_shared<Ui::SQM_client>())
 {
     mp_ui->setupUi(this);
+    using namespace dlib;
+    using namespace dlib::bayes_node_utils;
+    directed_graph<bayes_node>::kernel_1a_c bn;
 }
 
 SQM_client::~SQM_client()
